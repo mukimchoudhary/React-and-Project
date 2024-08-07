2b95bce209f107f383833d2e54816256bb2a01fe
Mohammad Mukim <mukimchoudharymg786@gmail.com>
2024-08-07 21:03:20 +0530

    currencygenerator

---
+++ 06currencygenerator/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <hi className='text-3xl bg-orange-500'>welcom on react after on week </hi>
+    </>
+  )
+}
+
+export default App
