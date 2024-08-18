bdd38ad9f6da46420940ca4972237fab349405f2
Mohammad Mukim <mukimchoudharymg786@gmail.com>
2024-08-18 08:06:25 +0530

    project

---
+++ 06currencyconverter/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <hi clsssName= 'text-6xl bg-orange-500'> currencyconverter on github</hi>
+    </>
+  )
+}
+
+export default App
